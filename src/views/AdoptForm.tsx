0d5eab2b2f0060a4b988669381f8e97eefd375fe
Miriam Asencio <148205684+Miritzila@users.noreply.github.com>
2024-11-12 17:16:57 +0000

    views añadidas

---
+++ src/views/AdoptForm.tsx
@@ -0,0 +1,9 @@
+const AdoptForm = () => {
+    return (
+        <div className="container text-center">
+            <h1>Formulario de Adopción</h1>
+        </div>
+    );
+}
+
+export default AdoptForm;
