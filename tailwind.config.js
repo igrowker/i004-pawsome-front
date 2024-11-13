ee9a2ec71f05ff39994847629ba1cc36de1efb24
Adrian <adrianalarcon720@gmail.com>
2024-11-13 19:49:43 +0100

    config tailwind colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6AB4A8",
+        secondary: "#835251",
+        accent1: "#B29F90",
+        accent2: "#D8C9B9",
+        dark: "#08121F",
+        light: "#F7FAFC",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
