ef547c86563d6cc324e1eb7436abcfd6c4163adc
Adrian <adrianalarcon720@gmail.com>
2024-11-13 20:19:06 +0100

    added colors to tailwind config + font family Roboto + sample on main.tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#6AB4A8",
-        secondary: "#835251",
-        accent1: "#B29F90",
-        accent2: "#D8C9B9",
+        primaryLight: "#6AB4A8",
+        primaryDark: "#44887b",
+        secondaryDark: "#B29F90",
+        secondaryLight: "#D8C9B9",
         dark: "#08121F",
         light: "#F7FAFC",
       },
       fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"], // ya lo tienes configurado correctamente
       },
+    },
+    // Cambiar la fuente por defecto para todos los elementos
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],  // Cambiar la fuente por defecto
     },
   },
   plugins: [],
