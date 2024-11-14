52c41a508881877837378c2019564fd44e087485
Melisa Navarro <151066758+Melsalome@users.noreply.github.com>
2024-11-15 00:15:45 +0100

    Styles - register

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import RegisterPage from "./views/RegisterPage";
 function App() {
   return (
 
-    <div className="">
-      <h1 className="bg-primaryLight p-5">PrimaryLight</h1>
-      <h1 className="bg-primaryDark p-5">PrimaryDark</h1>
-      <h1 className="bg-secondaryDark p-5">SecondaryLight</h1>
-      <h1 className="bg-secondaryLight p-5">SecondaryDark</h1>
-      <h1 className="bg-dark text-light p-5" >dark(backround), light(text)</h1>
-      <h1 className="bg-secondaryLight p-5">ROBOTO</h1>
-    </div> 
+    // <div className="">
+    //   <h1 className="bg-primaryLight p-5">PrimaryLight</h1>
+    //   <h1 className="bg-primaryDark p-5">PrimaryDark</h1>
+    //   <h1 className="bg-secondaryDark p-5">SecondaryLight</h1>
+    //   <h1 className="bg-secondaryLight p-5">SecondaryDark</h1>
+    //   <h1 className="bg-dark text-light p-5" >dark(backround), light(text)</h1>
+    //   <h1 className="bg-secondaryLight p-5">ROBOTO</h1>
+    // </div> 
+
+    <RegisterPage></RegisterPage>
   );
 }
 
--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -7,19 +7,19 @@ const RegisterForm = () => {
     </div>
         <form className = "max-w-md md:max-w-2xl lg:max-w-3xl mt-10 ml-[52px] flex flex-col justify-center" >
             <div className="email text-">
-                <input type="email" placeholder="Email" className="border-2 rounded-3xl h-14 w-[85%] mb-[25px]" required></input>
+                <input type="email" placeholder="Email" placeholder-gray-500 className="border-2 rounded-3xl h-14 w-[85%] mb-[25px] placeholder-black pl-2" required></input>
             </div>
             <div className="password">
-                <input type="password" placeholder="Contraseña" className="border-2 rounded-3xl h-14 w-[85%] mb-[25px]" required></input> 
+                <input type="password" placeholder="Contraseña" className="border-2 rounded-3xl h-14 w-[85%] mb-[25px] placeholder-black pl-2" required></input> 
             </div>
             <div className="password">
-                <input type="password" placeholder="Confirmar Contraseña" className="border-2 rounded-3xl h-14 w-[85%] mb-[25px]" required></input> 
+                <input type="password" placeholder="Confirmar Contraseña" className="border-2 rounded-3xl h-14 w-[85%] mb-[25px] placeholder-black pl-2" required></input> 
             </div>
             <div className="name">
-                <input type="text" placeholder="Nombre" className="border-2 rounded-3xl h-14 w-[85%] mb-[25px]" required></input>
+                <input type="text" placeholder="Nombre" className="border-2 rounded-3xl h-14 w-[85%] mb-[25px] placeholder-black pl-2" required></input>
             </div>
             <div className="lastName">
-            <input type="text" placeholder="Apellidos" className="border-2 rounded-3xl h-14 w-[85%] mb-[25px]" required></input>
+            <input type="text" placeholder="Apellidos" className="border-2 rounded-3xl h-14 w-[85%] mb-[25px] placeholder-black pl-2" required></input>
             </div>
             <div className="registerOptions inline-grid">
             <label>
