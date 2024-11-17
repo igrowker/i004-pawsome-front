e8e875d0f4450b268e93115b49d6e2b8c56a9ae6
CrisMachuca <cristinamachucam@gmail.com>
2024-11-17 23:16:25 +0100

    change styles md-screens

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,13 +10,14 @@ const Footer: React.FC = () => {
             <img src="/login.png" alt="Logo" className="w-16 h-16" />
             <h1 className="text-4xl font-bold">PAWSOME</h1>
           </div>
-          <p className="text-gray-400 text-sm md:mt-2">"Making every paw count."</p>
+          <p className="text-white text-sm text-center">"Making every paw count."</p>
         </div>
 
         {/* Social Media */}
-        <div className="text-center md:text-left">
+        <div className="text-center">
           <p className="text-md font-bold mb-4">Follow us on</p>
-          <div className="flex justify-center md:justify-start space-x-4">
+          <div className="w-12 h-1 bg-primaryLight mx-auto mb-5"></div>
+          <div className="flex justify-center space-x-4">
             <a
               href="https://facebook.com"
               target="_blank"
@@ -46,9 +47,9 @@ const Footer: React.FC = () => {
 
         {/* Quick Links */}
         <div>
-          <h2 className="text-lg font-bold text-gray-300 mb-4 text-center md:text-left">Quick Links</h2>
-          <div className="w-8 h-0.5 bg-primaryLight mx-auto md:mx-0 mb-5"></div>
-          <div className="grid grid-cols-2 gap-4">
+          <h2 className="text-lg font-bold text-gray-300 mb-4 text-center">Quick Links</h2>
+          <div className="w-12 h-1 bg-primaryLight mx-auto mb-5"></div>
+          <div className="grid grid-cols-2 gap-4 p-2">
             <a
               href="/about"
               className="block bg-gray-800 p-3 rounded-lg text-center hover:bg-gray-700 transition-all"
@@ -77,16 +78,16 @@ const Footer: React.FC = () => {
         </div>
 
         {/* Contact Us */}
-        <div className="text-center md:text-left space-y-4 md:col-span-3">
+        <div className="mt-8 text-center space-y-4 md:col-span-3 mx-auto">
           <h2 className="text-lg font-bold">Contact Us</h2>
-          <div className="w-8 h-0.5 bg-primaryLight mx-auto md:mx-0"></div>
+          <div className="w-12 h-1 bg-primaryLight mx-auto mb-5"></div> {/* Aseguramos que sea idéntica */}
           <div className="flex flex-col items-center md:items-start space-y-2">
             <div className="flex items-center space-x-2">
-              <FaPhone />
+              <FaPhone className="text-primaryLight" />
               <span>+34 655 555-555</span>
             </div>
             <div className="flex items-center space-x-2">
-              <FaEnvelope />
+              <FaEnvelope className="text-primaryLight" />
               <span>contact@pawsome.com</span>
             </div>
           </div>
@@ -95,11 +96,10 @@ const Footer: React.FC = () => {
 
       {/* Rights Reserved */}
       <div className="mt-8 border-t border-gray-700 pt-4 text-center text-sm text-gray-500">
-        © {new Date().getFullYear()} PAWSOME. All rights reserved.
+        © {new Date().getFullYear()} <b>PAWSOME.</b> All rights reserved.
       </div>
     </footer>
   );
 };
 
 export default Footer;
-
