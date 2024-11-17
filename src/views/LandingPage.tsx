32bb884d1677d41f2dc1af066c2f28474a88c0c1
David Padilla Villén <davidpadillavillen@gmail.com>
2024-11-17 19:40:32 +0000

    Added landing page

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -1,7 +1,14 @@
+import { FaArrowRight } from "react-icons/fa6";
+
+
 const LandingPage = () => {
     return (
         <div className="container text-center">
-            <h1>Landing Page</h1>
+            <h1>Pawsome</h1>
+            <h4>Un lugar, todos los refugios</h4>
+            <button className="bg-primaryLight text-light  font-semibold rounded-full shadow-md hover:bg-primaryDark focus:outline-none focus:ring-2 focus:ring-teal-400 focus:ring-opacity-75">
+                <FaArrowRight/>
+            </button>
         </div>
     );
 }
