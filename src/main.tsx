ea04c2bacfd2b2181911de19b9029e31908b3e78
scannapieco <diego.scannapieco@gmail.com>
2024-11-17 16:51:55 -0300

    Login

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/App.css'; // Asegúrate de que la ruta sea correcta
+import './styles/index.css'; // Asegúrate de que la ruta sea correcta
+import {BrowserRouter} from 'react-router-dom'
 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter><App/></BrowserRouter>
   </React.StrictMode>,
 )
