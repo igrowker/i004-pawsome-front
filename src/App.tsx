d38d639ac8f412823429a128d4810b14cc20a87d
Sarap13 <saraparra13@gmail.com>
2024-11-17 22:00:14 +0000

    WIP layout, already created in app.tsx but missing login, signin and some other routes yet. In dashboard route links to refugee, admin and user profiles and actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import AdoptForm from "./views/AdoptForm";
+import DonationList from "./views/DonationList";
+import Navbar from "./components/Navbar";
+import VolunteerForm from "./views/VolunteerForm";
+import DashboardRoutes from "./views/dashboard/DashboardRoutes";
+import Home from "./views/home/Home";
+import AnimalProfile from "./components/AnimalProfile";
+import LandingPage from "./views/LandingPage";
+import UsersRoutes from "./views/dashboard/user/UsersRoutes";
+import RefugeeRoutes from "./views/dashboard/refugee/RefugeeRoutes";
+
+
+
 function App() {
   return (
-    <div className="">
-      <h1 className="bg-primaryLight p-5">PrimaryLight</h1>
-      <h1 className="bg-primaryDark p-5">PrimaryDark</h1>
-      <h1 className="bg-secondaryDark p-5">SecondaryLight</h1>
-      <h1 className="bg-secondaryLight p-5">SecondaryDark</h1>
-      <h1 className="bg-dark text-light p-5" >dark(backround), light(text)</h1>
-      <h1 className="bg-secondaryLight p-5">ROBOTO</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/signin" element={<Singin />} /> */}
+          <Route path="/donationlist" element={<DonationList />} />
+          <Route path="/volunteerform" element={<VolunteerForm />} />
+          <Route path="/adoptform" element={<AdoptForm />} />
+          <Route path="/animalprofile" element={<AnimalProfile />} />
+
+          {/* Ruta del usuario refugio y sus subrutas */}
+          <Route path="/refugee" element={<RefugeeRoutes />} />
+
+          {/* Ruta del usuario adoptante? volunteer? y sus subrutas */}
+          <Route path="/user" element={<UsersRoutes />} />
+
+          {/* Ruta del dashboard y sus subrutas */}
+          <Route path="/dashboard/*" element={<DashboardRoutes />} />
+
+        </Routes>
+      </div>
+    </BrowserRouter >
+
   );
 }
 
--- src/views/dashboard/user/UsersRoutes.tsx
+++ src/views/dashboard/user/UsersRoutes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import UserProfile from "./pages/UserProfile";
+// import UserProfile from "./pages/UserProfile";
+import UserProfile from "../../../components/UserProfile";
 import Favorites from "./pages/Favorites";
 
 function UsersRoutes() {
   return (
     <Routes>
       <Route path="/" element={<UserProfile />} />
+      {/* Pendiente de hacer favorites ,no MVP */}
       <Route path="/favorites" element={<Favorites />} />
     </Routes>
   );
