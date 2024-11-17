fa2334f06d715daf50669905cc59a7824be2d04d
Mery Álvarez <mery.alvarez.h@gmail.com>
2024-11-17 09:49:36 +0100

    feat[donationList]

--- src/views/DonationList.tsx
+++ src/views/DonationList.tsx
@@ -1,25 +1,42 @@
+import { useEffect, useState } from "react";
+import { getDonationsData } from "./helpers/getDonations";
+
 const DonationList = () => {
+  const [donations, setDonations] = useState([]);
 
-    return (
+  useEffect(() => {
+    const data = async () => {
+      const donation = await getDonationsData();
+      setDonations(donation.results);
+    };
+    data();
+  }, []);
 
-        <div className="w-80 h-40 flex flex-col justify-center gap-2 rounded-2xl shadow p-2 mt-2 ms-2"        >
-            <div className="flex gap-2">
-                <img className="bg-neutral-500 w-24 h-24 shrink-0 rounded-full" alt="" />
-                <div className="flex flex-col">
-                    <span className="font-bold text-neutral-700 italic">Card title</span>
-                    <p className="line-clamp-3">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non
-                        dolor augue. Nunc dictum erat sit amet iaculis interdum. Ut neque
-                        tellus, congue vel lectus aliquam, dignissim porttitor velit.
-                    </p>
-                </div>
-            </div >
-            <button className="hover:bg-primaryLight bg-secondaryLight font-bold text-neutral-50 rounded-2xl p-2">
-                See more
-            </button>
+  return (
+    <div className="md:columns-2 sm:columns-1 ms-1">
+      {donations.map((item: any, id: number) => (
+        <div className="flex flex-col justify-center gap-2 rounded-2xl shadow p-2 mt-2 ms-2 ">
+          <div className="flex gap-2" key={id}>
+            <img
+              className="bg-neutral-500 w-24 h-24 shrink-0 rounded-full"
+              src={item.foto}
+              alt=""
+            />
+            <div className="flex flex-col">
+              <span className="font-bold text-neutral-700 italic">
+                {item.nombre}
+              </span>
+              <p className="line-clamp-3">{item.descripcion_corta}</p>
+              <p className="line-clamp-3">{item.valor_necesitado}€</p>
+              <button className="hover:bg-primaryLight bg-secondaryLight font-bold text-neutral-50 rounded-2xl p-2 w-44">
+                Donate
+              </button>
+            </div>
+          </div>
         </div>
-
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default DonationList;
