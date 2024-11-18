8cb99fa8eabfbe6e64395d826007d043f4b01484
Nico235711 <139637937+Nico235711@users.noreply.github.com.>
2024-11-18 12:32:05 -0300

    added a route to dashboard routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Login from './components/Login';
 import RegisterPage from './views/RegisterPage';
+import DashboardRoutes from './views/dashboard/DashboardRoutes';
 
 
 const App: React.FC = () => {
@@ -10,6 +11,7 @@ const App: React.FC = () => {
   return (
 
     <Routes>
+      <Route path='/' element={<DashboardRoutes />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<RegisterPage />}  ></Route>
     </Routes>
