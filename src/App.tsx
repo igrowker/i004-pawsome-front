ab0d6afc750ff7b2a7e1a00df9cb8ffbb0034d39
Nico235711 <139637937+Nico235711@users.noreply.github.com.>
2024-11-18 12:12:36 -0300

    git pull origin develop

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,12 @@ import RegisterPage from './views/RegisterPage';
 const App: React.FC = () => {
 
   return (
-    
-      <Routes>
+
+    <Routes>
       <Route path="/login" element={<Login />} />
-    {/* <div className="">
-      <h1 className="bg-primaryLight p-5">PrimaryLight</h1>
-      <h1 className="bg-primaryDark p-5">PrimaryDark</h1>
-      <h1 className="bg-secondaryDark p-5">SecondaryLight</h1>
-      <h1 className="bg-secondaryLight p-5">SecondaryDark</h1>
-      <h1 className="bg-dark text-light p-5" >dark(backround), light(text)</h1>
-      <h1 className="bg-secondaryLight p-5">ROBOTO</h1>
-    </div> */}
-    <Route path="/register" element={<RegisterPage />}  ></Route>
-    </Routes> 
-    
+      <Route path="/register" element={<RegisterPage />}  ></Route>
+    </Routes>
+
   );
 };
 
