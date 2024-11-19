7eb5f2f16fbadce4635ac2112ad3d3ab06a9fe73
Davpad <davidpadillavillen@gmail.com>
2024-11-19 11:57:04 +0100

    Applying styles in landing page

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -3,12 +3,14 @@ import { FaArrowRight } from "react-icons/fa6";
 
 const LandingPage = () => {
     return (
-        <div className="container text-center">
-            <h1>Pawsome</h1>
-            <h4>Un lugar, todos los refugios</h4>
-            <button className="bg-primaryLight text-light  font-semibold rounded-full shadow-md hover:bg-primaryDark focus:outline-none focus:ring-2 focus:ring-teal-400 focus:ring-opacity-75">
-                <FaArrowRight/>
-            </button>
+        <div className="flex flex-col items-bottom justify-center bg-gray-100 min-h-screen mt-12 pt-12">
+            <div className="text-center mt-12 pt-12">
+                <h1 className="text-5xl font-extrabold text-black mt-12 pt-12">Pawsome</h1>
+                <h4 className="text-black-500">Un lugar, todos los refugios</h4>
+                <button className="bg-primaryLight text-light text-4xl p-2 my-2 font-semibold rounded-full shadow-md hover:bg-primaryDark focus:outline-none focus:ring-2 focus:ring-teal-400 focus:ring-opacity-75">
+                    <FaArrowRight />
+                </button>
+            </div>
         </div>
     );
 }
