1083acbcf28704f78fb78c33679fa685292d68ec
Melisa Navarro <151066758+Melsalome@users.noreply.github.com>
2024-11-21 21:49:53 +0100

    Register succesfull

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,9 +1,7 @@
 import { useForm } from "react-form-ease";
 import { useState } from "react";
 import useRegister from "../hooks/useRegister";
-
-
-
+import { Spinner } from "./ui/spinner";
 
 const RegisterForm = () => {
   const [isSubmitted, setIsSubmitted] = useState(false); 
@@ -79,6 +77,7 @@ const RegisterForm = () => {
     setIsSubmitted(false);
   };
 
+  setTimeout 
 
   return (
     <>
@@ -96,6 +95,7 @@ const RegisterForm = () => {
             className="border-2 rounded-3xl h-14 w-[85%] mb-[25px] placeholder-black pl-2"
             value={formData.email}
             onChange={(e) => updateForm({ email: e.target.value })}
+            
           ></input>
           {formErrors.email && <p className="text-red-500">{formErrors.email}</p>}
         </div>
@@ -165,6 +165,7 @@ const RegisterForm = () => {
         Registrar
       </button>
       </form>
+      {isLoading && (<Spinner/>)}
       {isSubmitted  && isSuccess && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
           <div className="bg-white p-6 rounded-lg shadow-lg text-center">
@@ -181,9 +182,9 @@ const RegisterForm = () => {
       )}
 
   {formErrors && <p className="text-red-500">{apiError}</p>}
-  {isLoading && <p>Cargando...</p>}
     </>
   );
 };
 
 export default RegisterForm;
+
