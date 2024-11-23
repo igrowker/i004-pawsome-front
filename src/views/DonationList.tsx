dc5e0350299bf7d28aef3c367d97b66bae342fcd
Mery Álvarez <mery.alvarez.h@gmail.com>
2024-11-23 22:20:15 +0100

    donation kind change

--- src/views/DonationList.tsx
+++ src/views/DonationList.tsx
@@ -20,10 +20,7 @@ const DonationList: React.FC = () => {
   useEffect(() => {
     const data = async () => {
       try {
-        const response = await getDonationsData(); // La respuesta puede ser undefined o un objeto de tipo DonationsResponse
-        console.log(response); // Ver los datos recibidos en consola
-
-        // Si la respuesta es undefined o no tiene 'donations', usamos un array vacío
+        const response = await getDonationsData();
         if (response && Array.isArray(response.donations)) {
           const shelterFilter = response.donations.filter(
             (element: DonationInterface) => element.refugeId === 102);
@@ -32,11 +29,11 @@ const DonationList: React.FC = () => {
           console.error(
             "La respuesta no tiene la propiedad 'donations' correctamente."
           );
-          setDonations([]); // Usar un array vacío si no se encuentra la propiedad 'donations'
+          setDonations([]);
         }
       } catch (error) {
         console.error("Error al obtener los datos de donaciones:", error);
-        setDonations([]); // En caso de error, usar un array vacío
+        setDonations([]); 
       }
     };
     data();
@@ -64,10 +61,19 @@ const DonationList: React.FC = () => {
                 <p className="text-sm text-neutral-600 line-clamp-3">
                   {item.description}
                 </p>
+                {
+                  item.monetaryDonation? 
                 <p className="text-sm text-neutral-600 mt-2">
-                  Cantidad necesitada:{" "}
+                  Monto necesitado:{" "}
                   <span className="font-bold">{item.cuantityDonatio}€</span>
                 </p>
+                :
+                <p className="text-sm text-neutral-600 mt-2">
+                Cantidad necesitada:{" "}
+                <span className="font-bold">{item.donationNumber}</span>
+              </p>
+
+                }
                 <button className="mt-4 w-full bg-secondaryLight text-white font-bold py-2 px-4 rounded-lg hover:bg-primaryLight transition-colors">
                   Donate
                 </button>
@@ -82,29 +88,3 @@ const DonationList: React.FC = () => {
 
 export default DonationList;
 
-{
-  /* <div className="md:columns-2 sm:columns-1 ms-1 mt-20 mb-20">
-{donations.map((item: DonationInterface, id:number) => (
-  <div className="flex flex-col justify-center gap-2 rounded-2xl shadow p-2 mt-2 ms-2 h-auto">
-    <div className="flex gap-2" key={id}>
-      <img
-        className="bg-neutral-500 w-24 h-24 shrink-0 rounded-full"
-        src={item.imageUrl}
-        alt=""
-      />
-      <div className="flex flex-col">
-        <span className="font-bold text-neutral-700 italic mb-1">
-          {item.title}
-        </span>
-        <p className="line-clamp-3 mb-1">{item.description}</p>
-        <p className="line-clamp-3 mb-2"> Cantidad necesitada: {item.cuantityDonatio}€</p>
-        <button className="hover:bg-primaryLight bg-secondaryLight font-bold text-neutral-50 rounded-2xl p-2 w-44">
-          Donate
-        </button>
-      </div>
-    </div>
-  </div>
-))}
-</div>
- */
-}
