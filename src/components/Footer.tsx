64d76af36e6cc5b794964f70e3a527063488a84f
CrisMachuca <cristinamachucam@gmail.com>
2024-11-27 01:15:50 +0100

    added dark/light mode using tailwind in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,61 @@
-import { FaFacebook, FaInstagram, FaTwitter, FaPhone, FaEnvelope } from 'react-icons/fa';
+import { useState } from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaPhone,
+  FaEnvelope,
+  FaSun,
+  FaMoon,
+} from "react-icons/fa";
 
 const Footer: React.FC = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <footer className="bg-dark text-white py-8">
+    <footer
+      className={`py-8 transition-all ${
+        isDarkMode ? "bg-dark text-white" : "bg-gray-100 text-gray-800"
+      }`}
+    >
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 items-start">
         {/* Logo and name */}
         <div className="flex flex-col items-center md:items-start space-y-4">
-          <div className="flex items-center space-x-4">
-            <img src="/login.png" alt="Logo" className="w-16 h-16" />
-            <h1 className="text-4xl font-bold">PAWSOME</h1>
-          </div>
-          <p className="text-white text-sm text-center">"Un lugar, todos los refugios"</p>
+        <div className="flex items-center space-x-4">
+  <img src="/login.png" alt="Logo" className="w-16 h-16" />
+  <img
+    src={isDarkMode ? "/paw2.png" : "/paw.png"} // Cambia la imagen según el modo
+    alt="pawsome"
+    className="h-auto w-auto max-w-[200px] md:max-w-[250px] lg:max-w-[280px] max-h-[80px] md:max-h-[100px] lg:max-h-[120px] object-contain"
+  />
+</div>
+          <p className="text-sm text-center">
+            "Un lugar, todos los refugios"
+          </p>
         </div>
 
         {/* Social Media */}
         <div className="text-center">
           <p className="text-md font-bold mb-4">Síguenos en</p>
-          <div className="w-12 h-1 bg-primaryLight mx-auto mb-5"></div>
+          <div
+            className={`w-12 h-1 ${
+              isDarkMode ? "bg-primaryLight" : "bg-teal-500"
+            } mx-auto mb-5`}
+          ></div>
           <div className="flex justify-center space-x-4">
             <a
               href="https://facebook.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="p-3 text-primaryLight rounded-full hover:text-blue-600 transition-all"
+              className={`p-3 rounded-full transition-all ${
+                isDarkMode
+                  ? "text-primaryLight hover:text-blue-600"
+                  : "text-teal-500 hover:text-blue-400"
+              }`}
             >
               <FaFacebook className="text-lg" />
             </a>
@@ -30,7 +63,11 @@ const Footer: React.FC = () => {
               href="https://twitter.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="p-3 text-primaryLight rounded-full hover:text-blue-400 transition-all"
+              className={`p-3 rounded-full transition-all ${
+                isDarkMode
+                  ? "text-primaryLight hover:text-blue-400"
+                  : "text-teal-500 hover:text-blue-500"
+              }`}
             >
               <FaTwitter className="text-lg" />
             </a>
@@ -38,7 +75,11 @@ const Footer: React.FC = () => {
               href="https://instagram.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="p-3 text-primaryLight rounded-full hover:text-pink-600 transition-all"
+              className={`p-3 rounded-full transition-all ${
+                isDarkMode
+                  ? "text-primaryLight hover:text-pink-600"
+                  : "text-teal-500 hover:text-pink-500"
+              }`}
             >
               <FaInstagram className="text-lg" />
             </a>
@@ -47,55 +88,72 @@ const Footer: React.FC = () => {
 
         {/* Quick Links */}
         <div>
-          <h2 className="text-lg font-bold text-gray-300 mb-4 text-center">Enlaces rápidos</h2>
-          <div className="w-12 h-1 bg-primaryLight mx-auto mb-5"></div>
+          <h2 className="text-lg font-bold mb-4 text-center">Enlaces rápidos</h2>
+          <div
+            className={`w-12 h-1 ${
+              isDarkMode ? "bg-primaryLight" : "bg-teal-500"
+            } mx-auto mb-5`}
+          ></div>
           <div className="grid grid-cols-2 gap-4 p-2">
-            <a
-              href="/about"
-              className="block bg-gray-800 p-3 rounded-lg text-center hover:bg-gray-700 transition-all"
-            >
-              Sobre nosotros
-            </a>
-            <a
-              href="/signin"
-              className="block bg-gray-800 p-3 rounded-lg text-center hover:bg-gray-700 transition-all"
-            >
-              Registrarse
-            </a>
-            <a
-              href="/donations"
-              className="block bg-gray-800 p-3 rounded-lg text-center hover:bg-gray-700 transition-all"
-            >
-              Donaciones
-            </a>
-            <a
-              href="/privacy"
-              className="block bg-gray-800 p-3 rounded-lg text-center hover:bg-gray-700 transition-all"
-            >
-              Política de privacidad
-            </a>
+            {[
+              { href: "/about", label: "Sobre nosotros" },
+              { href: "/signin", label: "Registrarse" },
+              { href: "/donations", label: "Donaciones" },
+              { href: "/privacy", label: "Política de privacidad" },
+            ].map((link) => (
+              <a
+                key={link.href}
+                href={link.href}
+                className={`block p-3 rounded-lg text-center transition-all ${
+                  isDarkMode
+                    ? "bg-gray-800 hover:bg-gray-700"
+                    : "bg-gray-200 hover:bg-gray-300"
+                }`}
+              >
+                {link.label}
+              </a>
+            ))}
           </div>
         </div>
 
         {/* Contact Us */}
         <div className="mt-8 text-center space-y-4 md:col-span-3 mx-auto">
           <h2 className="text-lg font-bold">Contáctanos</h2>
-          <div className="w-12 h-1 bg-primaryLight mx-auto mb-5"></div>
+          <div
+            className={`w-12 h-1 ${
+              isDarkMode ? "bg-primaryLight" : "bg-teal-500"
+            } mx-auto mb-5`}
+          ></div>
           <div className="flex flex-col items-center md:items-start space-y-2">
             <div className="flex items-center space-x-2">
-              <FaPhone className="text-primaryLight" />
+              <FaPhone
+                className={`${isDarkMode ? "text-primaryLight" : "text-teal-500"}`}
+              />
               <span>+34 655 555-555</span>
             </div>
             <div className="flex items-center space-x-2">
-              <FaEnvelope className="text-primaryLight" />
+              <FaEnvelope
+                className={`${isDarkMode ? "text-primaryLight" : "text-teal-500"}`}
+              />
               <span>contacto@pawsome.com</span>
             </div>
           </div>
         </div>
       </div>
 
+      {/* Toggle Button for Dark/Light Mode */}
+      <div className="mt-8 text-center">
+        <button
+          onClick={toggleDarkMode}
+          className="flex items-center justify-center space-x-2 bg-primaryLight text-white px-4 py-2 mx-2 rounded-full shadow-md hover:bg-primaryDark transition-all"
+        >
+          {isDarkMode ? <FaSun className="text-yellow-400" /> : <FaMoon />}
+          <span>{isDarkMode ? "Modo Claro" : "Modo Oscuro"}</span>
+        </button>
+      </div>
+
       {/* Rights Reserved */}
-      <div className="mt-8 border-t border-gray-700 pt-4 text-center text-sm text-gray-500">
+      <div className="mt-8 border-t border-gray-700 pt-4 text-center text-sm">
         © {new Date().getFullYear()} <b>Pawsome.</b> Todos los derechos reservados.
       </div>
     </footer>
