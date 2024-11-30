46d096bfb7de28d211cf216b5e96a71ad27631f2
Jongabee <104025249+Jongabee@users.noreply.github.com>
2024-11-30 16:29:25 -0300

    connection with the back

--- src/components/ShelterList.tsx
+++ src/components/ShelterList.tsx
@@ -1,20 +1,11 @@
-import React from 'react';
-import ShelterCard from './ShelterCard';
+import React from "react";
+import ShelterCard from "./ShelterCard";
+import { IRefuge } from "@/interfaces/IRefugee";
 
-interface Shelter {
-  id: number;
-  name: string;
-  location: string;
-  timeAgo: string;
-  imageUrl: string;
-  profileUrl: string;
-  tags: string[];
-}
-
-const ShelterList: React.FC<{ shelters: Shelter[] }> = ({ shelters }) => (
+const ShelterList: React.FC<{ shelters: IRefuge[] }> = ({ shelters }) => (
   <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
     {shelters.map((shelter) => (
-      <ShelterCard key={shelter.id} shelter={shelter} />
+      <ShelterCard key={shelter._id} shelter={shelter} />
     ))}
   </div>
 );
