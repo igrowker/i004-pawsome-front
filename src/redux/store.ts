dae89125e8faeab15a86d761ead954d05490bb09
Jongabee <104025249+Jongabee@users.noreply.github.com>
2024-11-30 16:24:46 -0300

    fix typo

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,6 +2,7 @@ import {
   legacy_createStore as createStore,
   applyMiddleware,
   compose,
+  Reducer,
 } from "redux";
 import { thunk } from "redux-thunk";
 import rootReducer from "./rootReducer";
@@ -14,7 +15,10 @@ const persistConfig = {
   whitelist: ["auth"],
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(
+  persistConfig,
+  rootReducer as Reducer<Partial<RootState>>
+);
 declare global {
   interface Window {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
