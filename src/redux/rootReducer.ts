367335c9fed2c928da0d6e88a6d4e25246c14a99
Jongabee <104025249+Jongabee@users.noreply.github.com>
2024-11-30 16:25:54 -0300

    add refugeeReducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -5,6 +5,7 @@ import userReducer from "./reducers/userReducer";
 import { favoritesReducer } from "./reducers/favoritesReducer";
 import volunteeringReducer from "./reducers/volunteeringReducer";
 import notificationReducer from "./notificationSlice";
+import refugeeReducer from "./reducers/refugeeReducer";
 
 const rootReducer = combineReducers({
   animal: animalReducer,
@@ -13,6 +14,7 @@ const rootReducer = combineReducers({
   favorites: favoritesReducer,
   volunteering: volunteeringReducer,
   notifications: notificationReducer,
+  refugee: refugeeReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
